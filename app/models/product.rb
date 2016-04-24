fc1ef5a87b1dcd82c7ef7459727a36d12ae39e94
Mouhannad <mouhannad-ama@hotmail.com>
2016-04-24 17:28:02 +0800

    genrate scaffold for cart and line_items,make association between product and cart and line items

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
+
 	validates :title, :description, :img_url, :price, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness: true
@@ -7,5 +10,18 @@ class Product < ActiveRecord::Base
     message: 'must be a URL for GIF, JPG or PNG image.'
 	}
 	validates :title, length: {minimum: 10}
+
+	def self.latest
+		Product.order(:updated_at).last
+	end
+
+
+	def ensure_not_referenced_by_any_line_item
+		if line_items.blank?
+			true
+		else
+			false
+		end
+	end
 
 end
